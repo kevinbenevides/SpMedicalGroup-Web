d79c20a8f74e24fd0d67ac3d872f721a614348a0
Kevin Benevides <39235256801@info.local>
2019-06-13 11:22:23 -0300

    Updates

---
+++ precache-manifest.ac5363573c607515aeb54e31e4adb8d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "eef26b8b2f44572ca86e4f865646e58c",
+    "url": "/static/media/icon-login.eef26b8b.png"
+  },
+  {
+    "revision": "63c583cfe82918c579dbc93004f89a54",
+    "url": "/static/media/background3.63c583cf.jpg"
+  },
+  {
+    "revision": "2d2d2ec029398f7a6b6a6f31d746f818",
+    "url": "/static/media/background2.2d2d2ec0.jpg"
+  },
+  {
+    "revision": "9a7f554486aecd75d7c71f39d11a4bd5",
+    "url": "/static/media/background.9a7f5544.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "417efa92d52538e8adc2",
+    "url": "/static/js/main.03451b6c.chunk.js"
+  },
+  {
+    "revision": "a6aa643cdeeac357c58a",
+    "url": "/static/js/2.d9d27b5c.chunk.js"
+  },
+  {
+    "revision": "417efa92d52538e8adc2",
+    "url": "/static/css/main.ad804994.chunk.css"
+  },
+  {
+    "revision": "4b0b12a7592f9d840c0519e77767ba08",
+    "url": "/index.html"
+  }
+];
